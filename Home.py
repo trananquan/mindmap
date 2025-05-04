5555657a01f99daf2a88b6e9b2a23022278ab086
trananquan <130271615+trananquan@users.noreply.github.com>
2025-05-04 15:17:04 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Multi-App", layout="centered")
+st.sidebar.title("Navigation")
+st.sidebar.info("Use the sidebar to switch pages.")
+
+st.title("🏠 Home Page")
+st.write("Welcome to the AI mindmap !")
 
