ab83a16214c95daacaa25a2a2f02a677dbd8906a
trananquan <130271615+trananquan@users.noreply.github.com>
2025-05-05 12:11:38 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Multi-App", layout="centered")
 st.sidebar.title("Navigation")
 st.sidebar.info("Use the sidebar to switch pages.")
 
-st.title("🏠 AI-powered Mindmap Creator")
+st.markdown("<h1 style='color: darkblue;'>🏠 AI-powered Mindmap Creator</h1>", unsafe_allow_html=True)
 st.subheader("Welcome to the AI mindmap Toolkit Homepage!")
 st.image("images/app.1.jpg", use_container_width=True, caption="AI-generated Mindmap")
 st.subheader("Donation")
