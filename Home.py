0105a9df151db0fd0d880027ba35d8c6cd007c5b
trananquan <130271615+trananquan@users.noreply.github.com>
2025-05-05 11:51:51 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,5 +7,5 @@ st.sidebar.info("Use the sidebar to switch pages.")
 st.title("🏠 AI-powered Mindmap Creator")
 st.subheader("Welcome to the AI mindmap Toolkit Homepage!")
 st.image("images/app.1.jpg", use_container_width=True, caption="AI-generated Mindmap")
-image_url = "https://drive.google.com/file/d/1yE__OMnyxZwG2xdIo9MTwnqDRqTp2Gcp/view?usp=drive_link" 
+image_url = "https://drive.google.com/file/d/1g5pUd-fy-sRMtRJk4qfcHZPso6vj3dzU/view?usp=drive_link" 
 st.image(image_url, caption="Donation for AI Mindmap app")
