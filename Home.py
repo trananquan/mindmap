5eee311edc0486942b3659ed04fcd9c257ea0a6c
trananquan <130271615+trananquan@users.noreply.github.com>
2025-06-03 09:45:17 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,4 @@ st.sidebar.info("Use the sidebar to switch pages/ Sử dụng thanh công cụ �
 st.markdown("<h1 style='color: darkblue;'>🏠 AI-powered Mindmap Creator</h1>", unsafe_allow_html=True)
 st.subheader("Welcome to the AI mindmap Toolkit Homepage!")
 st.image("images/app.1.jpg", use_container_width=True, caption="AI-generated Mindmap")
-st.subheader("Donation/ Tài trợ")
-st.write("Donation for AI Mindmap app. Apply for Vietnamese bank account (180.000 VND ~ 7$)")
-st.write("Xin hãy tài trợ cho app để hoàn thiện thêm tính năng. Áp dụng cho tài khoản ngân hàng Việt Nam (180.000 VND ~ 7$)")
-st.image("images/Bidv_QR.jpg", width=300)
+
