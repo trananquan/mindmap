a520ac1135d267d15ca6fe01846d59e725477066
trananquan <130271615+trananquan@users.noreply.github.com>
2025-07-14 12:45:15 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="Multi-App",page_icon="", layout="centered")
+st.set_page_config(page_title="Multi-App",page_icon="🏠", layout="centered")
 st.sidebar.title("Navigation")
 st.sidebar.info("Use the sidebar to switch pages/ Sử dụng thanh công cụ để chuyển giữa các App.")
 
-st.markdown("<h1 style='color: darkblue;'> AI-powered Mindmap Creator</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color: darkblue;'>🏠AI-powered Mindmap Creator</h1>", unsafe_allow_html=True)
 st.subheader("Welcome to the AI mindmap Toolkit Homepage!")
 st.image("images/app.1.jpg", use_container_width=True, caption="AI-generated Mindmap")
 
