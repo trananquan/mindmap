9e7f50c474bd5de26c61328024fb61c955a8359c
trananquan <130271615+trananquan@users.noreply.github.com>
2025-08-23 13:16:40 +0700

    Update AI Text to Mindmap.py

--- pages/AI Text to Mindmap.py
+++ pages/AI Text to Mindmap.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import streamlit.components.v1 as components
 
 # Define the API key directly in the code
-API_KEY = "AIzaSyAD5-tRTbhtr17baOAVq307Fguv5oa49hY"
+API_KEY = "AIzaSyCSqGtk5Jt8RfMQQh8ySDzjY9R5lbxgcZ0"
 
 def configure_genai():
     """Configure the Gemini AI with the API key."""
