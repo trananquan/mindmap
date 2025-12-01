2b7c5916bc993ee2c4580aca520e9ae3ec31584c
trananquan <130271615+trananquan@users.noreply.github.com>
2025-12-01 15:08:43 +0700

    Change API key for Gemini AI
    Updated the API key for the Gemini AI configuration.

--- pages/AI Text to Mindmap.py
+++ pages/AI Text to Mindmap.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import streamlit.components.v1 as components
 
 # Define the API key directly in the code
-API_KEY = "AIzaSyCPCNt66Gnv8KX0QqToA0Zfrg6mxeA-J5c"
+API_KEY = "AIzaSyAlh5XgW2DestRDMH6YxmpDpwyh9LUzkPw"
 
 def configure_genai():
     """Configure the Gemini AI with the API key."""
