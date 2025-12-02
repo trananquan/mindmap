03afbbd2e7918c6640145446608afaa78945cee5
trananquan <130271615+trananquan@users.noreply.github.com>
2025-12-02 13:18:29 +0700

    Update AI Timeline Creator.py

--- pages/AI Timeline Creator.py
+++ pages/AI Timeline Creator.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 import json
 
 # Setup Gemini API
-genai.configure(api_key="AIzaSyAlh5XgW2DestRDMH6YxmpDpwyh9LUzkPw")
+genai.configure(api_key= st.secrets["GEMINI_API_KEY"])
 
 def get_events_from_gemini(description):
     prompt = f"""
