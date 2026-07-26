3239adfa783d9ef00893290d12b04e97ebd7d9ea
trananquan <130271615+trananquan@users.noreply.github.com>
2026-07-26 12:10:16 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -22,45 +22,30 @@ if "logged_in" not in st.session_state:
 def login():
 
     st.title("🔐 AI Mindmap Creator App")
-
-    st.markdown("### Login")
-
+    st.markdown("### Log in")
     username = st.text_input("Username")
-
     password = st.text_input(
         "Password",
         type="password"
     )
 
-    if st.button("Login"):
-
+    if st.button("Log in"):
         if username == USERNAME and password == PASSWORD:
-
             st.session_state.logged_in = True
-
             st.success("Login successful!")
-
             st.rerun()
-
         else:
-
             st.error("Incorrect username or password.")
-
 
 def home():
 
     st.sidebar.title("Navigation")
-
     st.sidebar.info(
         "Use the sidebar to switch pages."
     )
-
-    if st.sidebar.button("Logout"):
-
+    if st.sidebar.button("Log out"):
         st.session_state.logged_in = False
-
         st.rerun()
-
     st.markdown(
         "<h1 style='color: darkblue;'>🏠 AI-powered Mindmap Creator</h1>",
         unsafe_allow_html=True,
@@ -78,10 +63,7 @@ def home():
 
 
 if st.session_state.logged_in:
-
     home()
-
 else:
-
     login()
 
